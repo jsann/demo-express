3c51065bed52bce51fafd457686eee027f8eff25
JSANN <zenwink@gmail.com>
2015-03-07 12:23:54 +0800

    add movie list style

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 demo express
 ===
+1, install node.js<br>
+2, install mongodb && `mongod -dbpath xxx`<br>
+3, `npm install && node app`
--- public/script/default.js
+++ public/script/default.js
@@ -1,14 +1,43 @@
 $(function(){
 
-  var deleteItemBtn = $(".js-delete-item"),
-      userRegisterForm = $("form.register");
-      userLoginForm = $("form.login");
-      adminLoginForm = $("form.admin-login");
+  var movieListEnterTimeout;
 
+  var movieList = $(".movie-list"),
+      deleteItemBtn = $(".js-delete-item"),
+      loginOutBtn = $(".login-out"),
+      userRegisterForm = $("form.register"),
+      userLoginForm = $("form.login"),
+      adminLoginForm = $("form.admin-login"),
+      moviePostForm = $(".movie-post");
+
+  movieList.on("mouseenter", ".poster", movieListEnter);
+  movieList.on("mouseleave", ".poster", movieListLeave);
   deleteItemBtn.on("click", deleteItem);
+  loginOutBtn.on("click", loginOut);
   userRegisterForm.on("submit", userRegister);
   userLoginForm.on("submit", userLogin);
   adminLoginForm.on("submit", adminLogin);
+  moviePostForm.on("submit", moviePost);
+
+  function movieListEnter(){
+    var li = $(this).parents("li"),
+        detailModel = li.find(".detail-model"),
+        loading = li.find(".time-loading");
+    loading.find(".right").css({transition: "all 1s .2s", transform: "rotate(180deg)"});
+    loading.find(".left").css({transition: "all 1s 1s", transform: "rotate(180deg)"});
+    movieListEnterTimeout = setTimeout(function(){
+      detailModel.fadeIn(200);
+    }, 1800);
+  }
+
+  function movieListLeave(){
+    clearTimeout(movieListEnterTimeout);
+    var li = $(this).parents("li"),
+        detailModel = li.find(".detail-model"),
+        loading = li.find(".time-loading");
+    loading.find(".right, .left").css({transition: "none", transform: "rotate(0)"});
+    detailModel.fadeOut(200);
+  }
 
   /**
    * 删除 movie item
@@ -17,15 +46,30 @@ $(function(){
   function deleteItem(){
     var id = $(this).data("id");
     $.ajax({
-      url: "/api/admin/items/delete?id=" + id,
+      url: "/api/admin/movie/delete?id=" + id,
       type: "DELETE",
       success: function(data){
-        console.log(data)
+        console.log(data);
       },
       error: function(data){
-        console.log(data.responseText)
+        console.log(data.responseText);
       }
     })
+  }
+
+  function loginOut(){
+    $.ajax({
+      url: "/api/user/loginout",
+      type: "GET",
+      success: function(data){
+        if(data.result){
+          window.location.reload();
+        }
+      },
+      error: function(data){
+        console.log(data.responseText);
+      }
+    });
   }
 
   /**
@@ -89,4 +133,21 @@ $(function(){
       }
     });
   }
-})
+
+  function moviePost(event){
+    event.preventDefault();
+    $.ajax({
+      url: "/api/admin/movie/set",
+      type: "POST",
+      data: $(this).serializeObject(),
+      success:function(data){
+        if(data.result){
+          window.location.href = "/detail/#{data.data.id}";
+        }
+      },
+      error:function(data){
+        console.log(data.responseText);
+      }
+    });
+  }
+});
